ecf03719a4f5bdf9a29c7a32155170c521b55579
faiazparis <faiazparis@gmail.com>
2025-09-22 18:14:44 +0200

    Update 2

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -7,7 +7,7 @@ export default function Navigation() {
   const pathname = usePathname()
 
   const navItems = [
-    { href: '/', label: 'Bonjour' },
+    { href: '/', label: 'Home' },
     { href: '/cv', label: 'CV' },
     { href: 'https://x.com/faiazparis', label: 'X', external: true },
     { href: 'https://github.com/faiazparis', label: 'GitHub', external: true },
